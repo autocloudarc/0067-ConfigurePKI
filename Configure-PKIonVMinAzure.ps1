7a628f924136e76f73f4f92f561a006f3812c506
Preston K. Parsard <prestopa@microsoft.com>
2020-08-20 03:38:58 -0400

    Convert local modules list from string to array.

--- Configure-PKIonVMinAzure.ps1
+++ Configure-PKIonVMinAzure.ps1
@@ -72,14 +72,14 @@ param
     [string]$configName = "PkiConfig",
     [string]$aaaName = "aaa-1c5dce57-10",
     [string]$rgpName = "rg10",
-    [string[]]$modulesForAzureAutomation = @("ActiveDirectoryCSDsc","CertificateDsc","xPendingReboot","xStorage")
+    [string[]]$modulesForAzureAutomation = @("ActiveDirectoryCSDsc","CertificateDsc","xPendingReboot","xStorage"),
+    [string]$PSModuleRepository = "PSGallery"
 ) # end param
 
 $BeginTimer = Get-Date -Verbose
 
 #region MODULES
 # Module repository setup and configuration
-$PSModuleRepository = "PSGallery"
 Set-PSRepository -Name $PSModuleRepository -InstallationPolicy Trusted -Verbose
 Install-PackageProvider -Name Nuget -ForceBootstrap -Force
 
@@ -315,7 +315,9 @@ if ($proceed -eq "N" -OR $proceed -eq "NO")
 } #end if ne Y
 elseif ($proceed -eq "Y" -OR $proceed -eq "YES")
 {
-[string]$localModulesToInstall = @("Az","AzureAutomation")
+[string]$azurePreferredModule = "Az"
+[string]$azureNonPreferredModule = "AzureRM"
+[string[]]$localModulesToInstall = @("Az","AzureAutomation")
 # https://docs.microsoft.com/en-us/powershell/azure/new-azureps-module-az?view=azps-1.1.0
 Remove-ARMDeployPSModule -ModuleToRemove $azureNonPreferredModule -Verbose
 # Get required PowerShellGallery.com modules.
